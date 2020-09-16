554f57ee9783daf5ef9dac15db84bbc0235e7421
apereyra1 <70340835+apereyra1@users.noreply.github.com>
2020-09-15 21:57:55 -0400

    Create in class exercise_20200915.cs

---
+++ in class exercise_20200915.cs
@@ -0,0 +1,29 @@
+{
+    class Vehicle
+    {
+        int registration_Number;
+        string color;
+    }
+    class  Car : Vehicle
+    {
+        string make;
+        string type;
+        int model_Year;
+        int number_Doors;
+ 
+      class Bike : Vehicle
+    {
+        string make;
+        string type;
+        int number_of_gears;
+        
+    class Person : Vehicle
+    {
+        string owner_Name;
+    }
+
+    public static void Change_of_Owner();
+
+
+}
+}
